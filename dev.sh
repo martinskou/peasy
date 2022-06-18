8f67ed422416e8f590ff75ba8048c8fe7d24336b
Martin Drewes <msj@infoserv.dk>
2022-06-18 11:57:10 +0200

    comments

--- dev.sh
+++ dev.sh
@@ -1,4 +1,7 @@
 
+
+# setup ctrl-c to terminate the script by killing the go program.
+
 trap ctrl_c INT
 
 function ctrl_c() {
@@ -7,16 +10,34 @@ function ctrl_c() {
     exit 1
 }
 
+# make sure we have a place for the binary file.
+
 mkdir -p bin
 
-while true; do
-    go build -o bin/server src/* 
-    ./bin/server &
-    PID=$!
-    echo "Started with PID $PID in $PWD\n"
+EXEC="bin/server"
 
+# loop which is executed each time change to the source files are detected.
+
+while true; do
+    rm $EXEC 2>/dev/null
+    
+    go build -o $EXEC src/* # build the server
+
+    if [ -f "$EXEC" ]; then
+        # the new executeable was found, build was a success.
+        ./bin/server &
+        PID=$!
+        echo "Started with PID $PID in $PWD\n"
+    else
+        # no exec was found, something failed.
+        echo "Build failed"
+    fi
+
+    # wait for new changes to the source files.
     fswatch -1 -r -e ".*" -i "\\.go$" -i "\\.html$"  -i "\\.js$" .
 
+    # change detected, stop/wait for the process to terminate.
     kill $PID
     wait $PID 2>/dev/null
+
 done
